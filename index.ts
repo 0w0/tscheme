50aacf67ff7a6a74c67456b2c3332454df970885
0w0 <anonymous.0w0@gmail.com>
2016-08-21 03:28:48 +0800

    add evalate func, add deps: ramda.js, update .gitignore

--- index.ts
+++ index.ts
@@ -1,16 +1,26 @@
 // Ref: http://norvig.com/lispy.html
 // Using typescript as traspiler
 
-const input = "(set! twox (* x 2))"
-const input2 = "(define circle-area (lambda (r) (* pi (* r r))))"
-const input3 = "(begin (define r 10) (* pi (* r r)))"
+const globalEnv = {}
+// globalEnv["+"] = (a, b) => a + b
+// globalEnv["-"] = (a, b) => a - b
+// globalEnv["*"] = (a, b) => a * b
+// globalEnv["/"] = (a, b) => a / b
+// globalEnv[">"] = (a, b) => a > b
+// globalEnv["<"] = (a, b) => a < b
+// globalEnv[">="] = (a, b) => a >= b
+// globalEnv["<="] = (a, b) => a <= b
+// globalEnv["="] = (a, b) => a === b
+const ops = ["+", "-", "*", "/", ">", "<", ">=", "<=", "="].forEach(op => { globalEnv[op] = (a, b) => eval(`${a} ${op} ${b}`) })
+globalEnv["equal?"] = globalEnv["="]
+globalEnv["eq?"] = globalEnv["="]
+globalEnv["remainder"] = (a, b) => a % b
 
-const env = new Object()
 const tokenize = (input: string) => input.replace(/(\()|(\))/g, (_, a, b) => { if (a) { return `${a} `} else { return ` ${b}` } } ).split(" ")
 const atom = (token) => {
   const ret = Number(token)
 
-  return isNaN(ret) ? token : ret;
+  return isNaN(ret) ? token : ret
 }
 const parse = (tokens: Array<string>) => {
   if (tokens.length === 0) throw Error("Unexpected EOF while reading !")
@@ -30,16 +40,76 @@ const parse = (tokens: Array<string>) => {
   }
 }
 
+import * as R from "ramda"
+
+export default function evalate(s: string | number | Array<string>, tmpEnv?) {
+  const env = tmpEnv ? R.merge(globalEnv, tmpEnv) : globalEnv
+  // if (tmpEnv) console.log(s)
+  if (typeof s === 'string') {
+    return env[s]
+  } else if (typeof s === "number") {
+    return s
+  } else if (s[0] === "quote") {
+    return s.slice(1)
+  } else if (s[0] === "if") {
+    const [_, test, ret, or] = s
+    
+    return evalate(test) ? ret : or
+  } else if (s[0] === "define") {
+    const [_, name, exp] = s
+    env[name] = evalate(exp)
+  } else if (s[0] === "set!") {
+    const [_, name, exp] = s
+    env[name] = evalate(exp)
+  } else if (s[0] === "lambda") {
+    const [_, params, func] = s
+    const tmpEnv = {}
+    return (...args) => {
+      args.forEach((val, idx) => tmpEnv[params[idx]] = val)
+
+      return evalate(func, tmpEnv)
+    }
+  } else if (s[0] === "begin") {
+    // todo
+  } else {
+    const [op, ...args] = s
+    const operation = evalate(op, tmpEnv)
+    const subedArgs = args.map(arg => evalate(arg, tmpEnv))
+
+    return operation.apply(null, subedArgs)
+  }
+}
+
+const lamba1 = "(define area (lambda (r) (* 3.141592653 (* r r))))"
+const lamba2 = "(area 3)"
+
+const t1 = "(define r 10)"
+const t2 = "(* 3 (* r r))"
+const t3 = "(* 3 2)"
+
+evalate(parse(tokenize(lamba1)))
+const qa = evalate(parse(tokenize(lamba2)))
+
+console.log(qa)
+
+// console.log(env)
+// console.log(
+  // parse(tokenize(lamba1))
+// )
+
+// evalate(parse(tokenize(t1)))
+// console.log(env)
+// var q = evalate(parse(tokenize(t2)))
+// var q = evalate(parse(tokenize("12")))
+
+// console.log(q)
+
+// console.log(q)
+
+// evalate(parse(tokenize(t1)), env)
 // test
-console.log(
+// console.log(JSON.stringify(
   // tokenize(input3)
-  parse(tokenize(input3))
-)
-
-/*
-
-[ '(', 'set!', 'twox', '(', '*', 'x', '2', ')', ')' ] // no1 === ( 
-['set!', 'twox', '(', '*', 'x', '2', ')'] // return atom(tokens)
-['set!', 'twox', ['*', 'x', 2]]
-
-*/
+  // parse(tokenize(t1)),
+  // evalate(parse(tokenize(t3)), env)
+// ))
