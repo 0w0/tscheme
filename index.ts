d068d6b6e52d882eedc1849e3855a8efaa9f796b
0w0 <anonymous.0w0@gmail.com>
2016-08-20 16:12:35 +0800

    parse func

--- index.ts
+++ index.ts
@@ -1,12 +1,42 @@
+// Ref: http://norvig.com/lispy.html
+// Using typescript as traspiler
+
 const input = "(set! twox (* x 2))"
 
-const tokenize = (input: string) => input.replace(/(\()|(\))/g, (_, a, b) => {if(a) { return `${a} `} else { return ` ${b}` } }).split(" ")
-const parse = (token) => {}
+const env = new Object()
+const tokenize = (input: string) => input.replace(/(\()|(\))/g, (_, a, b) => { if (a) { return `${a} `} else { return ` ${b}` } } ).split(" ")
+const atom = (token) => {
+  const ret = Number(token)
 
+  return isNaN(ret) ? token : ret;
+}
+const parse = (tokens: Array<string>) => {
+  if (tokens.length === 0) throw Error("Unexpected EOF while reading !")
+  const no1 = tokens.shift()
+  if (no1 === "(") {
+    let list = []
+    while (tokens[0] !== ")") {
+      list.push(parse(tokens))
+    }
+    tokens.pop()
 
-
+    return list
+  } else if (no1 === ")") {
+    throw Error("Unexpected ) !")
+  } else {
+    return atom(no1)
+  }
+}
 
 // test
 console.log(
-  tokenize(input)
+  parse(tokenize(input))
 )
+
+/*
+
+[ '(', 'set!', 'twox', '(', '*', 'x', '2', ')', ')' ] // no1 === ( 
+['set!', 'twox', '(', '*', 'x', '2', ')'] // return atom(tokens)
+['set!', 'twox', ['*', 'x', 2]]
+
+*/
